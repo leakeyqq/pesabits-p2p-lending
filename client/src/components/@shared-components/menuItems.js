73337951d2688e2f4e9b59916d85e1ef2ae21e2b
anwar254 <sadatt254@gmail.com>
2024-11-13 00:57:23 +0300

    loan offers mod

--- client/src/components/@shared-components/menuItems.js
+++ client/src/components/@shared-components/menuItems.js
@@ -4,7 +4,7 @@ export const menuItems = [
   { title: "Dashboard", icon: LayoutDashboard, route: "/dashboard" },
 //   { title: "Management", icon: MessageSquare, route: "/management"},
 //   { title: "Calculator", icon: Calculator, route: "/calculator" },
-//   { title: "Cards", icon: CreditCard, route: "/cards" },
+  { title: "Cards", icon: CreditCard, route: "/cards" },
   { title: "Loan Offers", icon: Users, route: "/loan-offers" },
 //   { title: "Referrals", icon: Users, route: "/referrals" },
 //   { title: "Settings", icon: Settings, route: "/settings" },
